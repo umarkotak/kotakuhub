5ae3e99b9f4384e715b40cb6b77ffdb51026842f
umar.ramadhana <umar.ramadhana@gmail.com>
2021-11-09 13:12:09 +0700

    add adminlte skeleton

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Navbar from "../components/Navbar.svelte"
+  import Sidebar from "../components/Sidebar.svelte"
+</script>
+
+<div class="sidebar-mini layout-fixed layout-navbar-fixed dark-mode text-sm" data-new-gr-c-s-check-loaded="14.1038.0" data-gr-ext-installed="" style="height: auto;">
+  <div class="wrapper">
+    <Navbar />
+
+    <Sidebar />
+
+    <slot />
+  </div>
+</div>
---
+++ src/routes/index.svelte
@@ -0,0 +1,40 @@
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper" style="min-height: 818.047px;">
+  <!-- Content Header (Page header) -->
+  <div class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1 class="m-0">Dashboard</h1>
+        </div>
+        <!-- /.col -->
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item">
+              <a href="#">Home</a>
+            </li>
+            <li class="breadcrumb-item active">Dashboard v1</li>
+          </ol>
+        </div>
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
+    </div>
+    <!-- /.container-fluid -->
+  </div>
+  <!-- /.content-header -->
+  <!-- Main content -->
+  <section class="content">
+    <div class="container-fluid">
+    </div>
+    <!-- /.container-fluid -->
+  </section>
+  <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
+<footer class="main-footer">
+  <strong>Copyright © 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>. </strong> All rights reserved. <div class="float-right d-none d-sm-inline-block">
+    <b>Version</b> 3.1.0
+  </div>
+</footer>
